c1b444f0bca195d0ff83ce5ffbd653647682447c
Chris Makler <chris.makler@gmail.com>
2017-05-30 10:52:35 -0700

    Seeing whether it will work with the more standard run command

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
@@ -10,5 +9,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
