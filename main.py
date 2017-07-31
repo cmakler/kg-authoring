d24d7bfc76345b6cefc75e57cb86e1cbc3c452b3
Chris Makler <chris.makler@gmail.com>
2017-07-31 11:06:38 -0700

    some cleanup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 
 from viewObjects import point
 
@@ -14,17 +14,27 @@ def open_json_file(open_file):
         return json.load(datafile)
 
 
+def allow_cors(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
 @app.route('/')
-def hello_world():
-    return '<h1>Hello world!</h1>'
+def index():
+    return '<h1>KG Authoring</h1><p>KG Authoring is a web service that generates JSON readable by KGJS from more author-friendly formats.</p>'
 
 
 @app.route('/<name>', methods=['GET'])
 def test_json(name):
     c1 = open_json_file('data/'+name+'.json')
-    response = jsonify(c1)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return allow_cors(jsonify(c1))
+
+
+# Note: this will be replaced by an actual database someday
+@app.route('/data/<path:path>', methods=['GET'])
+def send_data(path=None):
+    directory = os.path.join(APP_ROOT, 'data/')
+    return allow_cors(send_from_directory(directory, path))
 
 
 if __name__ == '__main__':
